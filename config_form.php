d6e63d785df74d5217aa36a3ac847eb8b92a8287
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2022-07-22 12:41:27 +0200

    Fixed issues with serialize() function

--- config_form.php
+++ config_form.php
@@ -6,21 +6,25 @@
 	<div class="inputs five columns omega">
 		<p class="explanation"><?php echo __("Check the roles that will be able to see only their own Items (in Admin mode). <br>N.B.: by default, Super User's role is not available for limitation."); ?></p>
 		<div class="input-block">		
-			<?php 
-			$limitedRoles = unserialize(get_option('limit_visibility_to_own_items_roles'));
-			$userRoles = get_user_roles();
-			unset($userRoles['super']);
-			
-			echo '<ul style="list-style-type:none">';
-			foreach($userRoles as $role=>$label) {
-				echo '<li>';
-				echo $view->formCheckbox('limit_visibility_to_own_items_roles[]', $role,
-					array('checked'=> (count($limitedRoles) > 0 ? in_array($role, $limitedRoles) : false) ? 'checked' : '')
-				);		  
-				echo ' ' . __($label);
-				echo '</li>';
-			}   
-			echo '</ul>';
+			<?php
+				if (get_option('limit_visibility_to_own_items_roles') <> '0') {
+					$limitedRoles = unserialize(get_option('limit_visibility_to_own_items_roles'));
+				} else {
+					$limitedRoles = array();
+				}
+				$userRoles = get_user_roles();
+				unset($userRoles['super']);
+				
+				echo '<ul style="list-style-type:none">';
+				foreach($userRoles as $role=>$label) {
+					echo '<li>';
+					echo $view->formCheckbox('limit_visibility_to_own_items_roles[]', $role,
+						array('checked'=> (count($limitedRoles) > 0 ? in_array($role, $limitedRoles) : false) ? 'checked' : '')
+					);		  
+					echo ' ' . __($label);
+					echo '</li>';
+				}   
+				echo '</ul>';
 			?>
 		</div>
 	</div>
@@ -33,20 +37,24 @@
 		<p class="explanation"><?php echo __("Check the roles that will be able to see only their own Collections (in Admin mode). <br>N.B.: by default, Super User's role is not available for limitation."); ?></p>
 		<div class="input-block">		
 			<?php 
-			$limitedRoles = unserialize(get_option('limit_visibility_to_own_collections_roles'));
-			$userRoles = get_user_roles();
-			unset($userRoles['super']);
-			
-			echo '<ul style="list-style-type:none">';
-			foreach($userRoles as $role=>$label) {
-				echo '<li>';
-				echo $view->formCheckbox('limit_visibility_to_own_collections_roles[]', $role,
-					array('checked'=> (count($limitedRoles) > 0 ? in_array($role, $limitedRoles) : false) ? 'checked' : '')
-				);		  
-				echo ' ' . __($label);
-				echo '</li>';
-			}   
-			echo '</ul>';
+				if (get_option('limit_visibility_to_own_collections_roles') <> '0') {
+					$limitedRoles = unserialize(get_option('limit_visibility_to_own_collections_roles'));
+				} else {
+					$limitedRoles = array();
+				}
+				$userRoles = get_user_roles();
+				unset($userRoles['super']);
+				
+				echo '<ul style="list-style-type:none">';
+				foreach($userRoles as $role=>$label) {
+					echo '<li>';
+					echo $view->formCheckbox('limit_visibility_to_own_collections_roles[]', $role,
+						array('checked'=> (count($limitedRoles) > 0 ? in_array($role, $limitedRoles) : false) ? 'checked' : '')
+					);		  
+					echo ' ' . __($label);
+					echo '</li>';
+				}   
+				echo '</ul>';
 			?>
 		</div>
 	</div>
